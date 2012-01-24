cb6bed3f8e6f027f971725924c95dfc880b46bb1
Guilherme Ferreira <guilffer@gmail.com>
2012-01-24 00:40:19 -0200

    adicionando umas regex pra ficar mais bonito

--- sexp.js
+++ sexp.js
@@ -4,49 +4,58 @@ var sexp = function(text){
 };
 
 sexp.prototype = {
-    scan: function() {
-        var token = this.base[0] || null;
-        this.base = this.base.slice(1);
-        return token;
-    },
+  
+  slice_base: function(length) {
+    this.base = this.base.slice(length);
+  },
+  
+  scan: function() {
+    var token = this.base[0] || null;
+    this.slice_base(1)
+    return token;
+  },
 
-    parse: function(parent_) {
-        var list = parent_ || null;
-        var node = null;
+  parse: function(parent_) {
+    var list = parent_ || null;
+    var node = null;
+    
+    do {
+      var token = this.scan();
+      switch(token) {
+        case '(':
+          if (list == null)
+            list = this.parse([]);
+          else
+            list.push(this.parse([]));
+          break;
+          
+        case ')':
+          if (node != null) list.push(node);
+          return list;
+      
+        case ' ':
+          if (node != null) list.push(node);
+          node = null;
+          break;
         
-        do {
-          var token = this.scan();
-          switch(token) {
-              case '(':
-                if (list == null)
-                  list = this.parse([]);
-                else
-                  list.push(this.parse([]));
-                break;
-                
-              case ')':
-                if (node != null) list.push(node);
-                return list;
+        case '"':
+          node = this.base.match(/(.*?)\"/)[1];
+          this.slice_base(node.length+1);
+          break;
+        
+        case null: break;
+        
+        default:
+          //token += this.base.match(/([^\(\) \"]*)/)[0];
+          //this.slice_base(token.length-1);
+          node = (node == null) ? token : node+token;
+          
             
-              case ' ':
-                if (node != null) list.push(node);
-                node = null;
-                break;
-              
-              case '"':
-                node = '';
-                while((token = this.scan()) != '"')
-                  node += token;
-                break;
-              
-              default:
-                if (token != null)
-                    node = (node == null) ? token : node+token;
-                
-          }
-        } while(token != null);
-        return list;
-    }   
+      }
+    } while(token != null);
+    return list;
+  }   
+  
 };
 
 exports.sexp = sexp;
